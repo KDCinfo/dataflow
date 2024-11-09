374338b4c1a3ced333863fb6190c1da60bb7a708
Keith D Commiskey <atlassian@kdcinfo.com>
2024-11-08 23:34:55 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  header("Location: http://kdcinfo.com/app/dataflow/data_mapping_flow_analysis_tool/");
+  exit;
+?>
+<html>
+  <head><META HTTP-EQUIV=refresh content=0;URL=https://kdcinfo.com/app/dataflow/data_mapping_flow_analysis_tool/></head>
+</html>
