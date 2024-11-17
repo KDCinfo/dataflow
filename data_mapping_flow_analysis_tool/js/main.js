f3df1ae0299060dcdc8b9cec79c34c84e9c2f190
Keith D Commiskey <atlassian@kdcinfo.com>
2024-11-17 07:29:21 -0800

    Extracted `uiConfig` into its own file.

--- data_mapping_flow_analysis_tool/js/main.js
+++ data_mapping_flow_analysis_tool/js/main.js
@@ -1,48 +1,7 @@
 import AppSettings from './AppSettings.js';
+import { uiConfig } from './uiConfig.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const uiConfig = {
-    // @TODO: Extract IDs to a config file and add HTML element resolver.
-    ui: {
-      outputContainer: document.getElementById('outputContainer'),
-      clumpContainer: document.getElementById('clumpContainer'),
-    },
-    clumpPanel: {
-      clumpFormId: document.getElementById('clumpFormId'),
-      clumpNameInput: document.getElementById('clumpNameInput'),
-      clumpCodeInput: document.getElementById('clumpCodeInput'),
-      linkTo: document.getElementById('linkTo'),
-      columnSelect: document.getElementById('columnSelect'),
-      saveClumpButton: document.getElementById('clumpFormButtonSubmit')
-      // Not used
-      // resetClumpButton: document.getElementById('clumpFormButtonReset')
-    },
-    settings: {
-      gridRepeatRangeInput: document.getElementById('gridRepeatRangeInput'),
-      gridRepeatHtmlSpan: document.getElementById('gridRepeatHtmlSpan'),
-      gridRepeatSliderMarkers: document.getElementById('gridRepeatSliderMarkers'),
-    },
-    storage: {
-      exportDataButton: document.getElementById('exportDataButton'),
-      importDataButton: document.getElementById('importDataButton'),
-
-      storageNameTag: document.getElementById('storageNameTag'),
-      storageNameLabelCurrent: document.getElementById('storageNameLabelCurrent'),
-      newStorageNameInput: document.getElementById('newStorageNameInput'),
-      newStorageNameButton: document.getElementById('newStorageNameButton'),
-      storageButtonDelete: document.getElementById('storageButtonDelete'),
-      storageButtonUse: document.getElementById('storageButtonUse'),
-      storageNamingError: document.getElementById('storageNamingError')
-    },
-    infoPanel: {
-      lastAddedClumpIdTag: document.getElementById('lastAddedClumpIdTag'),
-      lastAddedColTag: document.getElementById('lastAddedColTag'),
-      editingIndexTag: document.getElementById('editingIndexTag'),
-      editingIdTag: document.getElementById('editingIdTag')
-    }
-  };
-
   const appSettings = new AppSettings(uiConfig);
-
   appSettings.loadData('/api/settings');
 });
---
+++ data_mapping_flow_analysis_tool/js/uiConfig.js
@@ -0,0 +1,38 @@
+export const uiConfig = {
+  ui: {
+    outputContainer: '#outputContainer',
+    clumpContainer: '#clumpContainer'
+  },
+  clumpPanel: {
+    clumpFormId: '#clumpFormId',
+    clumpNameInput: '#clumpNameInput',
+    clumpCodeInput: '#clumpCodeInput',
+    linkTo: '#linkTo',
+    columnSelect: '#columnSelect',
+    saveClumpButton: '#clumpFormButtonSubmit'
+    // Not used
+    // resetClumpButton: '#clumpFormButtonReset'
+  },
+  settings: {
+    gridRepeatRangeInput: '#gridRepeatRangeInput',
+    gridRepeatHtmlSpan: '#gridRepeatHtmlSpan',
+    gridRepeatSliderMarkers: '#gridRepeatSliderMarkers'
+  },
+  storage: {
+    exportDataButton: '#exportDataButton',
+    importDataButton: '#importDataButton',
+    storageNameTag: '#storageNameTag',
+    storageNameLabelCurrent: '#storageNameLabelCurrent',
+    newStorageNameInput: '#newStorageNameInput',
+    newStorageNameButton: '#newStorageNameButton',
+    storageButtonDelete: '#storageButtonDelete',
+    storageButtonUse: '#storageButtonUse',
+    storageNamingError: '#storageNamingError'
+  },
+  infoPanel: {
+    lastAddedClumpIdTag: '#lastAddedClumpIdTag',
+    lastAddedColTag: '#lastAddedColTag',
+    editingIndexTag: '#editingIndexTag',
+    editingIdTag: '#editingIdTag'
+  }
+};
